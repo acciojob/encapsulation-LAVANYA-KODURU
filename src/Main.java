967d09dc3194f61813c6ba2ec407101a3c71b561
Lavanya Lakshmi Koduru <141067145+LAVANYA-KODURU@users.noreply.github.com>
2025-01-11 21:46:44 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ package src;
 public class Main {
   public static void main(String[] args){
     RWOnly obj = new RWOnly();
+    //to check error
+    //obj.name="Test"; 
     obj.setName("Lavanya");
     System.out.println("Name: "+obj.getName());
   }
