4c855298db671386992a3fcf7221ac1726633953
Lavanya Lakshmi Koduru <141067145+LAVANYA-KODURU@users.noreply.github.com>
2025-01-11 21:51:36 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+      // Task 3: Create an object of RWOnly 
+      RWOnly obj = new RWOnly(); 
+        // Task 4: Try to set and access the name directly
+        // obj.name = "Test"; // Error: 'name' has private access in 'com.driver.RWOnly'
+        // Task 6: Use setter and getter
+        obj.setName("Lavanya"); // Setting value using setter
+        System.out.println("Name: " + obj.getName()); 
+    }
 }
