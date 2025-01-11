59e65d127ff2f628cee120ea2d124766a96dd81b
Lavanya Lakshmi Koduru <141067145+LAVANYA-KODURU@users.noreply.github.com>
2025-01-11 21:44:54 +0530

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+package src;
+public class Main {
+  public static void main(String[] args){
+    RWOnly obj = new RWOnly();
+    obj.setName("Lavanya");
+    System.out.println("Name: "+obj.getName());
+  }
+}
