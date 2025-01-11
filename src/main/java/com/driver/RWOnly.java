ec7808a4b2f7ce561cafce87e48f84c16e5561c1
Lavanya Lakshmi Koduru <141067145+LAVANYA-KODURU@users.noreply.github.com>
2025-01-11 21:54:13 +0530

    Update RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,12 @@
 package com.driver;
-
 public class RWOnly {
-    
+    private String name; // Task 2: Private member variable
+ // Task 5: Getter method
+    public String getName() {
+        return name;
+    }
+    // Task 5: Setter method
+    public void setName(String name) {
+        this.name = name;
+    }
 }
